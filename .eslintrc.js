27aae942344abdef8aff52483905445e4d79ea53
xdesign-louisecamlin <louise.camlin@xdesign.com>
2023-01-18 17:09:47 +0000

    add react version to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,11 @@ module.exports = {
   "plugins": [
     "react", "jest"
   ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    },
+  },
   "rules": {
     "indent": [
       "error",
