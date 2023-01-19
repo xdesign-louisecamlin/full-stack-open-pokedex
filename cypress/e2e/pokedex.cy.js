1dc1a60414f3f6d1d5b151406d989b9033d7761a
xdesign-louisecamlin <louise.camlin@xdesign.com>
2023-01-19 12:49:55 +0000

    add new cypress test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item').first().click()
+    cy.contains('bulbasaur')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
+
+  
